0c6fa72963388cdd5f1c27dbfefb99f629ef4cb0
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2023-12-12 14:41:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let p = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve('Hello, world!');
+	}, 1000)
+})
+
+p.then((data) => {
+	output.innerText = data;
+})
